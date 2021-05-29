5ecff30912198edbf7aecdf277cf21b2fbceb85f
Fernando Zegada <ferzeg3420@gmail.com>
2021-05-28 23:42:59 -0400

    Fixed a bug where I was never properly exiting out of a loop in makeArray. Added new functionality to make the tooltip appear above the focused thing.

--- tutorialMode.js
+++ tutorialMode.js
@@ -8,19 +8,18 @@ const HIDE_PREV_TOOLTIP_AFTER_FIRST = 1;
 const BETWEEN_BLACK_AND_INVISIBLE_SCREENS = 98;
 const FAIL_SAFE = 200;
 
-function makeArrayOfElementsWithIdSeq(idPrefix) {
+function makeArrayOfElementsWithClassSeq(idPrefix) {
     const array = [];
     for( let i = 1; i < FAIL_SAFE; i++ ) {
         let nextElmn = idPrefix + i;
-        let elmn = document.getElementById(nextElmn);
-        if( elmn === null ) {
+        let elmn = document.getElementsByClassName(nextElmn)[0];
+        if( elmn == null ) {
             break;
         }
         array.push(elmn);
     }
     return array;
 }
-
 
 document.body.insertAdjacentHTML("beforeend", 
     '<div id="translucent-black-cover"\
@@ -38,23 +37,36 @@ document.body.insertAdjacentHTML("beforeend",
           style="z-index: 1098"\
           class="fixed-on-bottom-center element-removed">\
           <h4>Click anywhere to continue</h4>\
-     </div>'
+     </div>\
+     <div id="tutorial-triangle"\
+          class="element-removed"\
+          style="position: absolute;\
+                 top: -10px;\
+                 left: 0;\
+                 z-index: 1097;\
+                 opacity: 0.9;\
+                 width: 0;\
+                 height: 0;\
+                 border-left: 15px solid transparent;\
+                 border-right: 15px solid transparent;\
+                 border-bottom: 15px solid cornflowerblue;">'
 );
 
 /* REFS */
 
-const infoTooltips = makeArrayOfElementsWithIdSeq("tutorial-text-");
+const infoTooltips = makeArrayOfElementsWithClassSeq("tutorial-text-");
 
 const yesButton = document.getElementById("start-tutorial");
 const noButton = document.getElementById("skip-tutorial");
 const closeButton = document.getElementById("close-tutorial-button");
+const triangle = document.getElementById("tutorial-triangle");
 
 const startTutorialBox = document.getElementById("tutorial-modal");
 const blackCover = document.getElementById("translucent-black-cover");
 const invisibleCover = document.getElementById("invisible-cover");
 const clickMessage = document.getElementById("click-message");
 
-const thingsToFocusOn = makeArrayOfElementsWithIdSeq("tutorial-focus-");
+const thingsToFocusOn = makeArrayOfElementsWithClassSeq("tutorial-focus-");
 
 /* FUNCTIONS */
 
@@ -86,13 +98,25 @@ function showCurrentTooltip(currentTooltip, currentTutorialElement) {
     return new Promise( resolve => {
         let currentTooltip = infoTooltips[indexOfCurrentTooltip];
         currentTooltip.classList.remove("element-removed");
-        
+        triangle.classList.remove("element-removed"); // removing so many times could be bad?
+
         let rect = currentTutorialElement.getBoundingClientRect();
+        if( currentTooltip.classList.contains("tutorial-text-above") ) {
+            let tooltipRect = currentTooltip.getBoundingClientRect();
+            triangle.style.transform = "rotate(180deg)";
+            triangle.style.top = String(rect.top - 20) + "px";
+            triangle.style.left = String(rect.left + 14) + "px";
+            currentTooltip.style.top = 
+                String(rect.top - 20 - (tooltipRect.bottom - tooltipRect.top))
+                + "px";
+            currentTooltip.style.left = String(rect.left + 10) + "px";
+            resolve('resolved');
+            return;
+        }
+        triangle.style.top = String(rect.bottom + 5.5) + "px";
+        triangle.style.left = String(rect.left + 14) + "px";
         currentTooltip.style.top = String(rect.bottom + 20) + "px";
         currentTooltip.style.left = String(rect.left + 10) + "px";
-        console.log("--- left:", rect.left);
-        console.log("--- bottom:", rect.bottom);
-
         resolve('resolved');
     });
 }
@@ -127,9 +151,13 @@ function endTutorial() {
     blackCover.classList.add("element-removed");
     invisibleCover.classList.add("element-removed");
     clickMessage.classList.add("element-removed");
+    triangle.classList.add("element-removed");
 
     for( let i = 0; i < infoTooltips.length; i++ ) {
         let elmn = infoTooltips[i];
+        if( elmn === undefined ) {
+            break;
+        }
         infoTooltips[i].classList.add("element-removed");
     }
 }
